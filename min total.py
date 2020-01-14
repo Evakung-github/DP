06cde090c0fd7a28fccabb5e22fc1fc9219ab18a
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-01-14 12:15:19 -0800

    Update min total.py

--- min total.py
+++ min total.py
@@ -4,6 +4,17 @@ Created on Tue Jan 14 11:32:50 2020
 
 @author: eva
 """
+# https://leetcode.com/problems/triangle/
+# task: Find the minimum sum of sequence of a triangle.
+
+# Instead of doing from top, I am doing the calculation from down.
+# Using dynamic programming, we are able to record the minimum sum value of each value after its level.
+# [
+#    [2],    -> [11]         ^
+#   [3,4],   -> [9,10]       |
+#  [6,5,7],  -> [7,6,10]     |
+# [4,1,8,3]  -> [4,1,8,3]    |
+#]
 
 class Solution:
     def minimumTotal(self, triangle):
