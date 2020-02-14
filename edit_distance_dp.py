9367a5c807ac450745c2642b75202c8a99fb515c
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-02-14 11:59:41 -0800

    Add files via upload

---
+++ edit_distance_dp.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 11:27:39 2020
+
+@author: eva
+"""
+
+class Solution:
+    def minDistance(self, s: str, t: str) -> int:
+        
+        dp = [[0]*(len(s)+1) for i in range((1+len(t)))]
+        
+        for i in range(1,len(s)+1):
+            dp[0][i] = i
+        for i in range(1,len(t)+1):
+            dp[i][0] = i
+        
+        for r in range(1,len(t)+1):
+            for c in range(1,len(s)+1):
+                if t[r-1] == s[c-1]:
+                    dp[r][c] = min(dp[r-1][c]+1,dp[r][c-1]+1,dp[r-1][c-1])
+                else:
+                    dp[r][c] = min(dp[r-1][c],dp[r][c-1],dp[r-1][c-1])+1
+        
+        return dp[-1][-1]
+                    
+        
+        
+        
+        
